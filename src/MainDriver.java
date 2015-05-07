f7c1e19d7b7a232a1c1afc9ddedbc4cdeaef3853
asifiqbal84 <asif.iqbal@msn.com>
2015-05-07 15:24:58 +0800

    another line for print

--- src/MainDriver.java
+++ src/MainDriver.java
@@ -1,7 +1,12 @@
+import java.lang.System;
+
 public class MainDriver{
     public static void main(String []){
         // this is print a line
         System.out.println("hello World");
+
+        System.out.println("this is second line");
     }
+
 
 }
